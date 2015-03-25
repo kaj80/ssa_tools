d74b1543f1246099d36e88f58554a2daf2507b7f
Sasha Kotchubievsky <sashakot@mellanox.com>
2015-03-25 14:33:48 +0200

    simple_build.sh: Add CFLAGS

--- jenkins/simple_build.sh
+++ jenkins/simple_build.sh
@@ -3,7 +3,7 @@ basedir=$1
 for dir in "$basedir"/*; do
     if test -d "$dir"; then
         echo $dir
-        cd $dir && ./autogen.sh && ./configure --prefix="/usr" && make -j && sudo make install
+	cd $dir && ./autogen.sh && ./configure CFLAGS="-g -O0 -rdynamic" --prefix="/usr" && make -j && sudo make install
         
     fi
 done

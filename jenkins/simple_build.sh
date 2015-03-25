20777c969d193793bed041888fcf878384b0cbf6
Sasha Kotchubievsky <sashakot@mellanox.com>
2015-03-25 14:25:16 +0200

    simple_build.sh: Use "/usr" prefix like MOFED uses.

--- jenkins/simple_build.sh
+++ jenkins/simple_build.sh
@@ -3,7 +3,7 @@ basedir=$1
 for dir in "$basedir"/*; do
     if test -d "$dir"; then
         echo $dir
-        cd $dir && ./autogen.sh && ./configure && make && sudo make install
+        cd $dir && ./autogen.sh && ./configure --prefix="/usr" && make && sudo make install
         
     fi
 done

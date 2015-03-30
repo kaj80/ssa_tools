02ab3fb1e7ff83cb44c8ce2873ee5184243b3674
Sasha Kotchubievsky <sashakot@mellanox.com>
2015-03-30 16:57:46 +0300

    simple_build.sh: Check return values of each build

--- jenkins/simple_build.sh
+++ jenkins/simple_build.sh
@@ -1,9 +1,15 @@
 #!/bin/bash   
 basedir=$1
+status=0
 for dir in "$basedir"/*; do
     if test -d "$dir"; then
         echo $dir
 	cd $dir && ./autogen.sh && ./configure CFLAGS="-g -O0 -rdynamic" --prefix="/usr" && make -j && sudo make install
-        
+	rc=$?
+	if [[ $rc -ne 0 ]]; then
+		echo "Failed to build $dir"
+		let status=status+rc
+	fi
     fi
 done
+exit $status

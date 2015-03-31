351b624b60fd0a473e08074635cc79867f27337d
Sasha Kotchubievsky <sashakot@mellanox.com>
2015-03-31 11:28:00 +0300

    Add pdsh timeout

--- jenkins/test_rsocket.sh
+++ jenkins/test_rsocket.sh
@@ -25,7 +25,8 @@ echo "$SERVER_PID"
 REMOTE_COMMAND="pkill -9 $TEST; $TEST -s $GID"
 echo $REMOTE_COMMAND
 
-pdsh -N -w "$REMOTE" "$REMOTE_COMMAND"
+pdsh -w $REMOTE "pkill -9 $TEST"
+pdsh -u 10 -N -w "$REMOTE" "$REMOTE_COMMAND"
 rc=$?;if [[ $rc != 0 ]]; then echo "Test failed"; fi
 
 kill -9 $SERVER_PID

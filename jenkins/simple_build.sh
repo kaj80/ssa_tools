e75815968e584ffe14cffcd2aa08fb1843203b90
Sasha Kotchubievsky <sashakot@mellanox.com>
2015-04-01 10:56:39 +0300

    simple_build.sh: Use default prefix

--- jenkins/simple_build.sh
+++ jenkins/simple_build.sh
@@ -4,7 +4,7 @@ status=0
 for dir in "$basedir"/*; do
     if test -d "$dir"; then
         echo $dir
-	cd $dir && ./autogen.sh && ./configure CFLAGS="-g -O0 -rdynamic" --prefix="/usr" && make -j && sudo make install
+	cd $dir && ./autogen.sh && ./configure CFLAGS="-g -O0 -rdynamic"  && make -j && sudo make install
 	rc=$?
 	if [[ $rc -ne 0 ]]; then
 		echo "Failed to build $dir"

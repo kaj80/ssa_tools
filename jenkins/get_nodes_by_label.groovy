f673cf9ce4a70b9bdeee75fe747508f8b35b5e86
Sasha Kotchubievsky <sashakot@mellanox.com>
2015-04-01 10:06:27 +0300

    Add get_nodes_by_label.groovy

---
+++ jenkins/get_nodes_by_label.groovy
@@ -0,0 +1,28 @@
+mport hudson.model.*
+
+String labelIWantServersOf = "SSA"; 
+String serverList = '';
+
+println(labelIWantServersOf);
+
+for (aSlave in hudson.model.Hudson.instance.slaves) {          
+  out.println('Evaluating Server(' + aSlave.name + ') with label = ' + aSlave.getLabelString());  
+
+  if (aSlave.getLabelString().indexOf(labelIWantServersOf ) > -1) {
+    serverList += aSlave.name + ' '+ aSlave.getLabelString() + '\n';        
+    out.println('Valid server found: ' + aSlave.name);                  
+  }    
+
+}
+
+channel = build.workspace.channel;
+
+fp = new hudson.FilePath(channel, build.workspace.toString() + "/nodes.list")
+
+if(fp != null)
+{
+    fp.write(serverList, null); //writing to file
+    String str = fp.readToString(); //reading from file
+  	out.println(str);
+}
+

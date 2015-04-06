3cc6c6f773e9ddbea7a30e5576d49ac0623125e8
Sasha Kotchubievsky <sashakot@mellanox.com>
2015-04-06 13:46:14 +0300

    simple_build.sh: Add parameter variable for make params

--- jenkins/simple_build.sh
+++ jenkins/simple_build.sh
@@ -1,10 +1,13 @@
-#!/bin/bash   
+#!/bin/bash
+
+make_opt="-j$(nproc)"
+
 basedir=$1
 status=0
 for dir in "$basedir"/*; do
     if test -d "$dir"; then
         echo $dir
-	cd $dir && ./autogen.sh && ./configure CFLAGS="-g -O0 -rdynamic"  && make -j && sudo make install
+	cd $dir && ./autogen.sh && ./configure CFLAGS="-g -O0 -rdynamic"  && make $make_opt && sudo make $make_op tinstall
 	rc=$?
 	if [[ $rc -ne 0 ]]; then
 		echo "Failed to build $dir"

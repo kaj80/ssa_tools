c69a2e775fedfb5df8080d24adcdb98784e6b885
Sasha Kotchubievsky <sashakot@mellanox.com>
2015-04-14 15:20:01 +0300

    ssa_coverity_check.sh: Use global make file for ibssa coverity check

--- ssa_coverity_check.sh
+++ ssa_coverity_check.sh
@@ -6,32 +6,22 @@ log=/tmp/install_$$.log
 
 function static {
     cov_bin_dir=/.autodirect/sysgwork/coverity/cov-sa-linux64-5.5.1/bin
-    coverity_dir=/tmp/coverity_$$
-    mkdir $coverity_dir
+    coverity_dir=${coverity_dir:="/tmp/coverity_$$"}
+    mkdir -p $coverity_dir
     pushd $coverity_dir
+    rm -rf ibssa2
     git clone https://github.com/hnrose/ibssa2.git
-    for lib in ibssa2/plugin ibssa2/distrib ibssa2/acm
-        do
-            echo "*************************  Checking $lib ***********************************"
-            pushd $lib
-            module=`basename $lib`
-            coverity_tmp_dir=`pwd`
-            make clean
-            make mostlyclean
-            make clean
-            make distclean
-            make maintainer-clean
-            git status
-            sync
-            sleep 2
-            ./autogen.sh 
-            ./configure
-            ${cov_bin_dir}/cov-build --dir ${coverity_tmp_dir} make -j4
-            ${cov_bin_dir}/cov-analyze --dir ${coverity_tmp_dir} 
-            ${cov_bin_dir}/cov-format-errors --dir ${coverity_tmp_dir} --emacs-style 2>&1|tee > coverity_$module.log
-            echo "FINISHED Checking $coverity_dir/$lib/coverity_$module.log ***********************************"
-            popd
-        done
+    echo "*************************  Checking SSA ***********************************"
+    module="SSA"
+    coverity_tmp_dir=ibssa2
+    pushd $coverity_tmp_dir
+    ./autogen.sh
+    ./configure
+    ${cov_bin_dir}/cov-build --dir ${coverity_tmp_dir} make -j4
+    ${cov_bin_dir}/cov-analyze --dir ${coverity_tmp_dir}
+    ${cov_bin_dir}/cov-format-errors --dir ${coverity_tmp_dir} --emacs-style 2>&1|tee > coverity_SSA.log
+    echo "FINISHED Checking $coverity_dir/coverity_SSA.log ***********************************"
+    popd
     popd    
 }
 

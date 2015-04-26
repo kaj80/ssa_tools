94c6ba230f83b46651749e196f2b408905ebdfd5
Sasha Kotchubievsky <sashakot@mellanox.com>
2015-04-26 12:53:26 +0300

    fix_valgrind_xml.sh: Add closing XML tag for valgrind entry
    Signed-off-by: Ilya Nelkenbaum <ilyan@mellanox.com>

--- jenkins/fix_valgrind_xml.sh
+++ jenkins/fix_valgrind_xml.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
+
+for f in $@; do
+	tail -2 $f | grep -q "</valgrindoutput>" || echo "</valgrindoutput>" >> $f
+done
+
 for f in $@; do
 	start_num=`cat $f | grep -c "<valgrindoutput>"`
 	end_num=`cat $f | grep -c "</valgrindoutput>"`
 
 	if [[ $start_num != $end_num ]]; then
-		echo "Fix $f"
+		echo "Fixing $f"
 		sed  -i -e '0,/<\/valgrindoutput>/{//d;}' $f
 	fi
 done

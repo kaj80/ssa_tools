1cbf05ebeaf8e578e3f1b7f76ef9d6ad857f6c38
Ilya Nelkenbaum <ilyan@mellanox.com>
2015-04-28 19:01:50 +0300

    jenkins/simple_build.sh: Fix typo
    Signed-off-by: Ilya Nelkenbaum <ilyan@mellanox.com>

--- jenkins/simple_build.sh
+++ jenkins/simple_build.sh
@@ -7,7 +7,7 @@ status=0
 for dir in "$basedir"/*; do
     if test -d "$dir"; then
         echo $dir
-	cd $dir && ./autogen.sh && ./configure CFLAGS="-g -O0 -rdynamic"  && make $make_opt && sudo make $make_op tinstall
+	cd $dir && ./autogen.sh && ./configure CFLAGS="-g -O0 -rdynamic"  && make $make_opt && sudo make $make_op install
 	rc=$?
 	if [[ $rc -ne 0 ]]; then
 		echo "Failed to build $dir"

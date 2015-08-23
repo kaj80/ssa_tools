28bf672e11920d6db5960da73d9671325651832b
Roy Menczer <roym@mellanox.com>
2015-08-23 17:03:29 +0300

    Add initial versions of 2 scripts for run_ssa job
    Initial version of 2 scripts to be used by run_ssa jenkins job.
    One configures the ip addresses of hosts.
    The other creates file to be pre-loaded by ssa core
    
    Signed-off-by: Roy Menczer <roym@mellanox.com>

---
+++ jenkins/gen_host_data.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+input_file="${input_file:-/tmp/jenkins/workspace/run_ssa/label/dev-r-vrt-030/nodes.list}"
+
+hostname_file="${/tmp/hostnames.list}"
+
+output_file="${/etc/rdma/ibssa_hosts.data}"
+
+cat $input_file | awk '{print $1}' > $hostname_file
+
+echo ""  | tr -d '\n' > $output_file
+
+while read line; do
+
+	ssh -n $line '
+
+	output_directory=/etc/rdma
+	ip_file=/tmp/ip
+	flags_file=/tmp/flags
+	QP_file=/tmp/QP
+	GID_file=/tmp/GID
+
+	mkdir -p $output_directory
+
+	if ibportstate -D 0 1 | grep -q "LinkUp"; then
+
+		port_name=ib0
+
+	elif ibportstate -D 0 2 | grep -q "LinkUp"; then
+
+		port_name=ib1
+
+	fi
+	
+	
+	ip address show dev $port_name | grep "inet " | awk '\''{print $2}'\'' | cut -f1 -d'\''/'\'' > $ip_file
+
+	(echo "0x" ; ip address show dev $port_name | grep infiniband | awk '\''{print $2}'\'' | cut -f1 -d'\'':'\'' ; ) | tr -d '\''\n'\'' > $flags_file
+
+	(echo "0x" ; ip address show dev $port_name | grep infiniband | awk '\''{print $2}'\'' | tr - : | cut -f2-4 -d'\'':'\'' ; ) | tr -d '\'':\n'\'' > $QP_file
+
+	ibaddr | awk '\''{print $2}'\'' > $GID_file
+
+	( ( cat /tmp/ip ; echo -e '\''\t\t'\'' ; cat /tmp/GID ; echo -e  '\''\t'\'' ; cat /tmp/QP ; echo -e '\''\t'\'' ; cat /tmp/flags ) | tr -d '\''\n'\''  ; echo '\'''\'' ; )
+	' >> $output_file
+
+done < $hostname_file
+
+
+
+while read line; do
+
+	scp $output_file root@$line:$output_file
+
+done < $hostname_file
+
+exit 0

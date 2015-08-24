3ad62401806d6246f33d756f61dc4c555852fe21
Sasha Kotchubievsky <sashakot@mellanox.com>
2015-08-24 19:32:31 +0300

    Next commit

--- tests/ssa_test.sh
+++ tests/ssa_test.sh
@@ -9,11 +9,13 @@ RDMA_CONF_DIR=${RDMA_CONF_DIR:="/sashakot/usr/etc/rdma/"}
 CORE_CONF=${CORE_CONF:-"$RDMA_CONF_DIR/ibssa_core_opts.cfg"}
 ADDR_DATA_FILE=${ADDR_DATA_FILE:-"$RDMA_CONF_DIR/ibssa_hosts.data"}
 OPENSM_DEFAULT_PRIORITY=7
+SSADMIN_TIMEOUT=${SSADMIN_TIMEOUT:-" "}
 CORE_LOG=${CORE_LOG:-"/var/log/ibssa.log"}
 LOCAL_PORT_NUM=0
 SWITCH_GID_FOR_RESET=""
 let SWITCH_PORT_FOR_RESET=0
-LOCAL_GID=""
+CORE_GID=""
+CORE_LID=""
 DISTRIB_GID=""
 DISTRIB_LID=""
 ACCESS_GID=""
@@ -25,10 +27,11 @@ ACM_LID=""
 function error_exit
 {
 	echo "$PROGRAME: ${1:-"Unknow Error"}" 1>&2
+	exit 1
 }
 
 
-function get_rtrn()
+function get_rtrn
 {
 	if (( $# < 2 )); then
 		error_exit "ERROR - ${FUNCTION} wrong number of parameters"
@@ -36,13 +39,13 @@ function get_rtrn()
 	echo `echo $1 | cut --delimiter=, -f $2`
 }
 
-start_core ()
+function start_core
 {
 	sudo pkill -9 opensm
 	sudo $OPENSM -F $OPENSM_CORE_CONF -B --priority $OPENSM_DEFAULT_PRIORITY
 }
 
-get_local_port_number ()
+function get_local_port_number
 {
 	if  ibportstate -D 0 1 | grep -q "LinkUp"; then
 		let LOCAL_PORT_NUM=1
@@ -51,23 +54,37 @@ get_local_port_number ()
 	fi
 }
 
-get_local_gid ()
+function get_local_gid
 {
-	LOCAL_GID=`sudo ibaddr | cut -f2 -d" "`; rc=$?
-	if (( $rc != 0 )) || [[ -z $LOCAL_GID ]]; then
+	local tmp=`sudo ibaddr`; rc=$?
+
+	if (( $rc != 0 )); then
 		error_exit "ERROR - can't get local GID"
 	fi
+
+	CORE_GID=`echo $tmp | cut -f2 -d" "`
+	if [[ -z $CORE_GID ]]; then
+		error_exit "ERROR - can't get local GID"
+	fi
+
+	local  hex_lid=`echo $tmp | cut -f5 -d" " `
+	CORE_LID=`printf '%d' $hex_lid`
 }
 
-generate_sm_hadover ()
+function generate_sm_hadover
 {
-	sudo ibportstate -D 0 $LOCAL_PORT_NUM reset
+	local -i rc
+	sudo ibportstate -D 0 $LOCAL_PORT_NUM reset; rc=$?
+
+	return $rc
 }
 
-find_port_for_reset ()
+function find_port_for_reset
 {
-	SWITCH_GID_FOR_RESET=`sudo ibnetdiscover --Switch_list | head -1 | cut -f3 -d" "`
-	if [[ -z $SWITCH_GID_FOR_RESET ]]; then
+	local -i rc
+
+	SWITCH_GID_FOR_RESET=`sudo ibnetdiscover --Switch_list | head -1 | cut -f3 -d" "`; rc=$?
+	if (( $rc != 0 )) || [[ -z $SWITCH_GID_FOR_RESET ]]; then
 		error_exit "ERROR - switch is not found"
 	fi
 
@@ -89,21 +106,33 @@ find_port_for_reset ()
 
 	echo $SWITCH_PORT_FOR_RESET
 	echo $SWITCH_GID_FOR_RESET
+
+	return 0
 }
 
-generate_pr_update ()
+function generate_pr_update
 {
-	ibportstate --Guid $SWITCH_GID_FOR_RESET $SWITCH_PORT_FOR_RESET reset >> /dev/null
+	local -i rc
+
+	ibportstate --Guid $SWITCH_GID_FOR_RESET $SWITCH_PORT_FOR_RESET reset >> /dev/null; rc=$?
+
+	if (( $rc != 0 )); then
+		error_exit "ERROR -ibportstate failed"
+	fi
+
+	return 0
 }
 
-generate_ip_update ()
+function generate_ip_update
 {
 	touch $ADDR_DATA_FILE
 	sudo pkill -HUP opensm
 }
 
-find_down_node ()
+function find_down_node
 {
+	local -i rc=0
+
 	if [[ $# == 0 ]]; then
 		error_exit "ERROR - ${FUNCTION} called without input parameters"
 	fi
@@ -112,13 +141,13 @@ find_down_node ()
 		error_exit "ERROR - ${FUNCTION} the first parameter should be valid IB GID"
 	fi
 
-	local down_connection=`sudo $SSADMIN -g $1 --format=down nodeinfo | head -1`
-	if [[ -z $down_connection ]]; then
+	local down_connection=`$SSADMIN $SSADMIN_TIMEOUT -g $1 --format=down nodeinfo | head -1`
+	if (( $rc != 0 )) || [[ -z $down_connection ]]; then
 		error_exit "ERROR - there is no downstream connection"
 	fi
 
 	local let down_node_lid=`echo $down_connection | cut -f2 -d" "`; rc=$?
-	if (( $rc != 0)) || [[ 0 == $down_node_lid ]]; then
+	if (( $rc != 0)) || (( 0 > $down_node_lid )); then
 		error_exit "ERROR - there is no downstream connection"
 	fi
 
@@ -127,9 +156,9 @@ find_down_node ()
 		error_exit "ERROR - there is no downstream connection"
 	fi
 
-	local down_node_type=`sudo $SSADMIN  -t -1 -g $down_node_gid  --format=short nodeinfo`; rc=$?
+	local down_node_type=`$SSADMIN $SSADMIN_TIMEOUT -g $down_node_gid --format=short nodeinfo`; rc=$?
 	if (( $rc != 0 )) || [[ -z $down_node_type ]]; then
-		error_exit "ERROR - can't access node "$down_node_type
+		error_exit "ERROR - can't access node "$down_node_gid
 	fi
 
 	if echo $down_node_type | grep -q "Core"; then
@@ -143,10 +172,14 @@ find_down_node ()
 	fi
 
 	echo $down_node_lid,$down_node_gid,$down_node_type
+
+	return 0
 }
 
-get_epochs ()
+function get_epochs
 {
+	local -i rc
+
 	if [[ $# == 0 ]]; then
 		error_exit "ERROR - ${FUNCTION} called without input parameters"
 	fi
@@ -155,7 +188,7 @@ get_epochs ()
 		error_exit "ERROR - ${FUNCTION} the first parameter should be valid IB GID"
 	fi
 
-	local epochs=`sudo $SSADMIN -g $1 stats DB_EPOCH IPV4_EPOCH IPV6_EPOCH NAME_EPOCH`; rc=$?
+	local epochs=`sudo $SSADMIN $SSADMIN_TIMEOUT -g $1 stats DB_EPOCH IPV4_EPOCH IPV6_EPOCH NAME_EPOCH`; rc=$?
 
 	if (( $rc != 0 )) || [[ -z $epochs ]]; then
 		error_exit "ERROR - can't access node "$1
@@ -167,11 +200,14 @@ get_epochs ()
 	local let name_epoch=`echo "$epochs" | grep "NAME_EPOCH" | cut -f2 -d" "`
 
 	echo $db_epoch,$ipv4_epoch,$ipv6_epoch,$name_epoch
+
+	return 0
 }
 
 
-find_ssa_nodes ()
+function find_ssa_nodes
 {
+	local -i rc=0
 	DISTRIB_GID=""
 	DISTRIB_LID=""
 	ACCESS_GID=""
@@ -182,18 +218,18 @@ find_ssa_nodes ()
 	local lid
 	local gid
 	local t
-	local result
+	local result=""
 
 	let lid=0
 	let ACM_LID=0
 	let ACCESS_LID=0
 	let DISTRIB_LID=0
 
-	result=`find_down_node $LOCAL_GID`
+	result=`find_down_node $CORE_GID`
 
-	if [[ -z $result ]]; then
-		echo "WARNING - a node don't have any downstream connection "$LOCAL_GID
-		return
+	if (( $rc != 0 )) || [[ -z $result ]]; then
+		echo "WARNING - a node don't have any downstream connection "$CORE_GID
+		return 1
 	fi
 
 	let lid=`get_rtrn $result 1`
@@ -202,7 +238,6 @@ find_ssa_nodes ()
 
 	if [[ -z $t ]] || [[ -z $gid ]] || (( $lid < 0)); then
 		error_exit "ERROR - Wrong ssadmin output "$result
-
 	fi
 
 	if [[ $t == "Core" ]]; then
@@ -223,14 +258,15 @@ find_ssa_nodes ()
 		ACM_GID=$gid
 		let ACM_LID=$lid
 
-		return
+		return 1
 	fi
 
-	result=`find_down_node $gid`
+	result=""
+	result=`find_down_node $gid`; rc=$?
 
-	if [[ -z $result ]]; then
+	if (( $rc != 0 )) || [[ -z $result ]]; then
 		echo "WARNING - a node don't have any downstream connection "$gid
-		return
+		return 1
 	fi
 
 	let lid=`get_rtrn $result 1`
@@ -255,14 +291,15 @@ find_ssa_nodes ()
 		ACM_GID=$gid
 		let ACM_LID=$lid
 
-		return
+		return 1
 	fi
 
-	result=`find_down_node $gid`
+	result=""
+	result=`find_down_node $gid`; rc=$?
 
-	if [[ -z $result ]]; then
+	if (( $rc != 0 )) || [[ -z $result ]]; then
 		echo "WARNING - a node don't have any downstream connection "$gid
-		return
+		return 1
 	fi
 
 	let lid=`get_rtrn $result 1`
@@ -282,31 +319,61 @@ find_ssa_nodes ()
 		ACM_GID=$gid
 		let ACM_LID=$lid
 
-		return
+		return 1
+	fi
+
+	return 0
+}
+
+function get_iner_nodes_epochs ()
+{
+	local tmp
+
+	tmp=`get_epochs $CORE_GID`
+	echo "Core "$tmp
+
+	if [[ ! -z $DISTRIB_GID ]];then
+		tmp=`get_epochs $DISTRIB_GID`
+		echo "Distrib "$tmp
+	fi
+
+	if [[ ! -z $ACCESS_GID ]];then
+		tmp=`get_epochs $ACCESS_GID`
+		echo "Access "$tmp
+	fi
+
+	if [[ ! -z $ACM_GID ]];then
+		tmp=`get_epochs $ACM_GID`
+		echo "ACM "$tmp
+	fi
+}
+
+function print_ssa_nodes ()
+{
+	echo "Core "$CORE_GID" "$CORE_LID
+
+	if [[ ! -z $DISTRIB_GID ]];then
+		echo "Distrib "$DISTRIB_GID" "$DISTRIB_LID
+	fi
+
+	if [[ ! -z $ACCESS_GID ]];then
+		echo "Access "$ACCESS_GID" "$ACCESS_LID
+	fi
+
+	if [[ ! -z $ACM_GID ]];then
+		echo "ACM "$ACM_GID" "$ACM_LID
 	fi
 }
 
 get_local_port_number
-get_local_gid
+get_local_gid; rc=$?
+if (( rc != 0 )); then
+	exit $rc
+fi
 #start_core
 find_port_for_reset
-generate_pr_update
-generate_ip_update 
 find_ssa_nodes
+print_ssa_nodes
 
-echo "Distrib "$DISTRIB_GID" "$DISTRIB_LID
-echo "Access "$ACCESS_GID" "$ACCESS_LID
-echo "ACM "$ACM_GID" "$ACM_LID
-
-get_epochs $LOCAL_GID
-if [[ ! -z $DISTRIB_GID ]];then
-	get_epochs $DISTRIB_GID
-fi
-
-if [[ ! -z $ACCESS_GID ]];then
-	get_epochs $ACCESS_GID
-fi
-
-if [[ ! -z $ACM_GID ]];then
-	get_epochs $ACM_GID
-fi
+generate_pr_update
+generate_ip_update

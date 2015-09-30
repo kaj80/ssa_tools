e5378e0ace56328754fe7a9b69ec16828586ad17
Roy Menczer <roym@mellanox.com>
2015-09-30 17:41:34 +0300

    ip_config.sh: Add configuration of IPv6 addresses
    Signed-off-by: Roy Menczer <roym@mellanox.com>

--- jenkins/ip_config.sh
+++ jenkins/ip_config.sh
@@ -5,6 +5,7 @@ SSA_HOSTS_ADDR_FILE="${SSA_HOSTS_ADDR_FILE:-ssa.hosts.ipaddr}"
 
 cat $SSA_HOSTS_ADDR_FILE | while read line; do
 	ip=$(echo $line | awk '{print $2}')
+	ipv6=$(echo $line | awk '{print $3}')
 	host=$(echo $line | awk '{print $1}')
 
 	sudo pdsh -w $host "
@@ -18,6 +19,40 @@ cat $SSA_HOSTS_ADDR_FILE | while read line; do
 			ifconfig ib1 $ip netmask 255.255.0.0
 
 		fi
+
+		echo $ipv6 > /tmp/my_ipv6
 	"
-	#echo "ifconfig $interface $ip netmask 255.255.0.0"
+
+	sudo pdsh -w $host '
+
+
+		if ibportstate -D 0 1 | grep -q LinkUp; then
+
+			interface=ib0
+
+		else
+
+			interface=ib1
+
+		fi
+
+		correct_addr=`cat /tmp/my_ipv6`
+
+		has_correct_addr=0
+
+		suffix=/64
+
+		correct_addr=$correct_addr$suffix
+
+		ip addr show dev $interface | grep '\''inet6'\'' | while read line; do
+
+			ipv6_addr=`echo $line | awk '\''{print $2}'\''`
+
+			ip -6 addr del $ipv6_addr dev $interface
+
+		done
+
+		ip -6 addr add $correct_addr dev $interface
+
+	'
 done

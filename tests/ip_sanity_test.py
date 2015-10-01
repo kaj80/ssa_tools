9b7036d5d12fd9a3f08611462e173c91fe69831a
Roy Menczer <roym@mellanox.com>
2015-10-01 15:14:40 +0300

    ip_sanity_test.py: Add test #2, partial code for other tests
    Signed-off-by: Roy Menczer <roym@mellanox.com>

--- tests/ip_sanity_test.py
+++ tests/ip_sanity_test.py
@@ -8,6 +8,54 @@ except Exception as e:
 	print e
 	sys.exit(1)
 
+def compare_epoch_dictionaries(old_dictionary, new_dictionary, epoch_type):
+
+	status = 0
+
+	for nt in old_dictionary.keys():
+		for ng in old_dictionary[nt].keys():
+			e_old = int(old_dictionary[nt][ng])
+			e_new = int(new_dictionary[nt][ng])
+
+			print '-I- %s / %s - %s epoch: old %d new %d' % \
+			      (nt, ng, epoch_type, e_prev, e_curr)
+
+			if e_new != (e_old + 1):
+				print '-E- Bad %s epoch on %s node (%s)' % (ng, nt)
+				status = 1
+				break
+		if status != 0:
+			break
+
+	return status
+
+def compare_timestamp_dictionaries(old_dictionary, new_dictionary, excluded_types): #FIXME
+
+	status = 0
+
+	for nt in old_dictionary.keys():
+		for ng in old_dictionary[nt].keys():
+			tstamp_old = old_dictionary[nt][ng]
+			tstamp_new = new_dictionary[nt][ng]
+
+			#FIXME - add print??
+
+			if (nt in excluded_types):
+				if tstamp_old != tstamp_new:
+					print "-E- Bad timestamp on %s node (%s - shouldn't update)" \
+						% (ng, nt)
+					status = 1
+					break
+			else:
+				if tstamp_old >= tstamp_new:
+					print "-E- Bad timestamp on %s node (%s)" \
+						% (ng, nt)
+					status = 1
+					break
+		if status != 0:
+			break
+	return status
+
 def test_ip_update():
 	status = 0
 
@@ -23,51 +71,130 @@ def test_ip_update():
 		print e
 		status = 1
 
-	for nt in ipv4_epochs.keys():
-		for ng in ipv4_epochs[nt].keys():
-			e_prev = int(ipv4_epochs[nt][ng])
-			e_curr = int(ipv4_epochs_new[nt][ng])
-
-			print '-I- %s / %s - IPv4 epoch: old %d new %d' % \
-			      (nt, ng, e_prev, e_curr)
-
-			if e_curr != (e_prev + 1):
-				status = 1
-				break
+	status = compare_epoch_dictionaries(ipv4_epochs, ipv4_epochs_new, IPv4)
 
 	if status != 0:
-		print '-E- Bad IPv4 epoch on %s node (%s)' % (ng, nt)
 		return status
 
-	for nt in ipv6_epochs.keys():
-		for ng in ipv6_epochs[nt].keys():
-			e_prev = int(ipv6_epochs[nt][ng])
-			e_curr = int(ipv6_epochs_new[nt][ng])
+	status = compare_epoch_dictionaries(ipv6_epochs, ipv6_epochs_new, IPv6)
 
-			print '-I- %s / %s - IPv6 epoch: old %d new %d' % \
-			      (nt, ng, e_prev, e_curr)
+	return status
 
-			if e_curr != (e_prev + 1):
-				status = 1
-				break
+def test_pr_update():
 
-	if status != 0:
-		print '-E- Bad IPv6 epoch on %s node (%s)' % (ng, nt)
-		return status
+	status = 0
 
-	return 0
+	try:
+		old_db_epochs = ip_flow_api.get_db_epochs('DB_EPOCH')
 
-def main(argv):
+		ip_flow_api.generate_pr_update()
+
+		new_db_epochs = ip_flow_api.get_db_epochs('DB_EPOCH')
+
+	except Exception as e:
+		print e
+		return 1
+
+	status = compare_epoch_dictionaries(old_db_epochs, new_db_epochs, DB)
+
+	return status
+
+def test_SM_handover(): #FIXME
+
+	status = 0
+
+	try:
+		old_update_times = ip_flow_api.get_last_db_upadte_time()
+
+		#FIXME - add code for bouncing SM port here
+
+		new_update_times = ip_flow_api.get_last_db_update_time()
+
+	except Exception as e:
+		print e
+		return 1
+
+	status = compare_timestamp_dictionaries(old_update_times, new_update_times, \
+						(core_nodes))
+
+	return status
+
+def test_distrib_failover(): #FIXME
+
+	status = 0
+
+	try:
+		old_db_epochs = ip_flow_api.get_db_epochs('DB_EPOCH')
+
+		#FIXME - add code that makes distribution fail
+
+		new_db_epochs = ip_flow_api.get_db_epochs('DB_EPOCH')
+
+	except Exception as e:
+		print e
+		return 1
+
+	status = compare_epoch_dictionaries(old_db_epochs, new_db_epochs, DB)
+
+	return status
+
+def test_access_failover(): #FIXME
+
+	status = 0
+
+	try:
+		old_update_times = ip_flow_api.get_last_db_upadte_time()
+
+		#FIXME - add code that makes access fail  here
+
+		new_update_times = ip_flow_api.get_last_db_update_time()
+
+	except Exception as e:
+		print e
+		return 1
+
+	status = compare_timestamp_dictionaries(old_update_times, new_update_times, ())
+
+	return status
+
+def main(argv): #FIXME
 
 	print '-I- IP update test START'
-
 	res = test_ip_update()
 	if res != 0:
 		print '-E- IP update test FAILED'
 		sys.exit(1)
-
 	print '-I- IP update test PASSED'
 
+	print '-I- PR update test START'
+	res = test_pr_update()
+	if res != 0:
+		print '-E- PR update test FAILED'
+		sys.exit(1)
+	print '-I- PR update test PASSED'
+
+	print '-I- SM handover test START'
+	res = test_sm_handover()
+	if res != 0:
+		print '-E- SM handover test FAILED'
+		sys.exit(1)
+	print '-I- SM handover test PASSED'
+
+	print '-I- Distribution failover test START'
+	res = test_distrib_failover()
+	if res != 0:
+		print '-E- Distribution failover test FAILED'
+		sys.exit(1)
+	print '-E- Distribution failover test FAILED'
+
+	print '-I- Access failover test START'
+	res = test_access_failover()
+	if res != 0:
+		print '-E- Access failover test FAILED'
+		sys.exit(1)
+	print '-I- Access failover test PASSED'
+
+	#FIXME: add ret value?
 
 if __name__ == "__main__":
         main(sys.argv[1:])

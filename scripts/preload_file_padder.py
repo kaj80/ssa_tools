564e7dbabeae1887f6b981b01ef139245a048c27
Roy Menczer <roym@mellanox.com>
2015-10-15 16:13:13 +0300

    preload_file_padder.py: Add options and option parsing

--- scripts/preload_file_padder.py
+++ scripts/preload_file_padder.py
@@ -1,20 +1,91 @@
 #!/usr/bin/python
 
-# This script is meant to drastically increase the size
+# This script is meant to increase the size
 # of the ibssa_hosts.data file preloaded by the core
-# it's input is the number of addresses to add
-# it outputs that many IPv4 addresses and that many IPv6 addresses
+# it outputs an equal amount IPv4 addresses and IPv6 addresses
 # and the expected size of this addresses
 
 import sys
 import time
+from optparse import OptionParser
 
 GID = 'fe80::2:c902:26:31d9'
 QP = '0x000048'
 flags = '0x80'
+ipv4_prefix = '100.0.'
+amount = None
 
 ipv4_arr=[2,0]
 ipv6_arr=[1,0]
+
+def parse_opts():
+	
+	global GID
+	global QP
+	global flags
+	global ipv4_prefix
+	global amount
+
+	parser = OptionParser()
+
+	parser.add_option('-g','--gid', dest ='GID', \
+			  help = ('Provide GID to be used for output ' \
+				+ 'preload file entries'))
+
+	parser.add_option('--qp', dest = 'QP', \
+			  help = ('provide QP to be used for '\
+				 + 'output preload file entries'))
+
+	parser.add_option('-f', '--flags', dest = 'flags', \
+			 help = ('provide flags to be used ' \
+				+ 'for output preload file entries'))
+
+	parser.add_option('-p', '--prefix', dest = 'ipv4_prefix', \
+			  help = ('provide a prefix for the ouput ipv4 addresses.\n' + \
+				'must be of the format \'x.y.\' where 0 <= x,y <= 255'))
+
+	parser.add_option('-a', '--amount', type = 'int', dest = 'amount', \
+			  help = 'provide desired amount of IPv4 and IPv6 addresses')
+
+	(options, _) = parser.parse_args()
+
+	if options.amount == None:
+		print 'ERROR: desired amount of addresses must be given.' \
+			+ ' use \'-a <desired amount>\''
+		return False
+	amount = options.amount
+
+	if options.ipv4_prefix != None:
+		if check_ipv4_prefix(options.ipv4_prefix):
+			ipv4_prefix = options.ipv4_prefix
+		else:
+			print 'ERROR: IPv4 prefix must be of the format' \
+				+ ' \'x.y.\' where 0 <= x,y <= 255'
+			return False
+	if options.GID != None:
+		GID = options.GID
+	if options.QP != None:
+		QP = options.QP
+	if options.flags != None:
+		flags = options.flags
+
+	return True
+
+def check_ipv4_prefix(prefix):
+
+	lst = prefix.split('.')
+	if len(lst) != 3:
+		return False
+	if lst[2] != '':
+		return False
+	try:
+		n1 = int(lst[0])
+		n2 = int(lst[1])
+	except:
+		return False
+	if n1 > 255 or n1 < 0 or n2 > 255 or n2 < 0:
+		return False
+	return True
 
 def inc_ipv4_arr(): # needs to be changed if we want input larger than 65023
 
@@ -48,19 +119,21 @@ def main (argv):
 
 	time_str = time.strftime("%H:%M:%S %d %b %Y")
 
-	num = int(argv[0])
-	if num > 65024:
+	if not parse_opts():
+		return
+
+	if amount > 65024:
 		print 'ERROR: size is to large.\ninput must be smaller than 65025\n'
 		return
-	size = (float(num * 72)) / 1024
+	size = (float(amount * 72)) / 1024
 	print ('# %d additional lines added by preload_file_padder.py on ' \
-		% num + time_str)
+		% amount + time_str)
 	print '# The size of an IPv4 record is 32 bytes'
 	print '# The size of an IPv6 record is 40 bytes'
 	print '# Expected total size of additional addresses is %.3fKB' % size
 
-	for i in range(num):
-		ipv4_addr = '100.0.' + str(ipv4_arr[0]) + '.' + str(ipv4_arr[1])
+	for i in range(amount):
+		ipv4_addr = ipv4_prefix + str(ipv4_arr[0]) + '.' + str(ipv4_arr[1])
 		print '%-30s %s\t\t%s\t%s' % (ipv4_addr, GID, QP, flags)
 		inc_ipv4_arr()
 
